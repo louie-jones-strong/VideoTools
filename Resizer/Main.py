dd814fc1eea45d2b07d0884b06a477cc3414c90f
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2021-08-22 12:55:31 +0100

    resize videos

--- Resizer/Main.py
+++ Resizer/Main.py
@@ -1,37 +1,60 @@
 from PIL import Image
 import os, sys
+import moviepy.editor as mp
 
-def ResizeImage(path, newResolution):
+
+def ResizeImage(path, newResolutions):
 	img = Image.open(path)
-	newImg = img.resize(newResolution, Image.ANTIALIAS)
 
-	filePath, extension = os.path.splitext(path)
-	newImg.save(filePath + "_" + str(newResolution[0]) + "x" + str(newResolution[1]) + ".png", "png")
+	for newResolution in newResolutions:
+		newImg = img.resize(newResolution, Image.ANTIALIAS)
 
+		newImg.save(NewAddress(path, newResolution, ".png"), "png")
 	return
 
-def ResizeFolder(path, newResolution):
+def ResizeVideo(path, newResolutions):
+	video = mp.VideoFileClip(path)
 
-	files = os.listdir(path)
+	for newResolution in newResolutions:
+		newVideo = video.resize(height=newResolution[0], width=newResolution[1])
+
+		newVideo.write_videofile(NewAddress(path, newResolution, ".mp4"))
+	return
+
+def NewAddress(path, newResolution, extension):
+	filePath, extension = os.path.splitext(path)
+	return filePath + "_" + str(newResolution[0]) + "x" + str(newResolution[1]) + extension
+
+def ResizeFolder(path, files, newResolutions):
 
 	for file in files:
 
 		address = os.path.join(path, file)
+
 		if os.path.isfile(address):
-			ResizeImage(address, newResolution)
+			filePath, extension = os.path.splitext(address)
+
+			if extension == ".png":
+				ResizeImage(address, newResolutions)
+			elif extension == ".mp4":
+				ResizeVideo(address, newResolutions)
 
 		else:
-			ResizeFolder(address, newResolution)
+			files = os.listdir(address)
+			ResizeFolder(address, files, newResolutions)
 
 	return
 
 
 if __name__ == "__main__":
 
+	print("agrs: " + str(sys.argv))
+
 	path = sys.argv[1]
 
-	newResolution = (int(sys.argv[2]), int(sys.argv[3]))
-
 	print("path: " + path)
-	print("newResolution: " + str(newResolution))
-	ResizeFolder(path, newResolution)
+	files = os.listdir(path)
+
+	newResolutions = [(1920, 1080), (960, 540), (480, 270)]
+	print("newResolution: " + str(newResolutions))
+	ResizeFolder(path, files, newResolutions)
